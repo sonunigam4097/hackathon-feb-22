042c73414e4bc5e1af4a685a1ecc458e22ba35fa
AmarjithTK <amarjithraveendran@gmail.com>
2023-02-04 16:02:46 +0530

    done

--- flask/urlvalidator/func.py
+++ flask/urlvalidator/func.py
@@ -21,18 +21,18 @@ def isValidUrl(url):
 
 
 def getUrlResponses(urlist):
-    range1 = range(100,199)
-    range2 = range(200,299)
-    range3 = range(300,399)
-    range4 = range(400,499)
-    range5 = range(500,599)
+    # range1 = range(100,199)
+    # range2 = range(200,299)
+    # range3 = range(300,399)
+    # range4 = range(400,499)
+    # range5 = range(500,599)
 
-    list = {
-        "one":[],
-        "two":[],
-        "three":[],
-        "four":[],
-        "five":[],
+    resplist = {
+        "success":[],
+        "fail":[],
+        # "three":[],
+        # "four":[],
+        # "five":[],
 
     }
 
@@ -46,18 +46,22 @@ def getUrlResponses(urlist):
         respcode = sp.get(each,headers={"User-Agent":"Mozilla/5.0","Accept-Language":"en-US"}).status_code
         sp.close()
         print(respcode)
-        if respcode in range1 :
-            list["one"].append(each)
-        elif respcode in range2 :
-            list["two"].append(each)   
-        elif respcode in range3 :
-            list["three"].append(each)   
-        elif respcode in range4 :
-            list["four"].append(each)   
-        elif respcode in range5 :
-            list["five"].append(each)                       
+        if respcode in range(200,399):
+            resplist["success"].append(each)
+        else:
+            resplist["fail"].append(each)    
+        # if respcode in range1 :
+        #     list["one"].append(each)
+        # elif respcode in range2 :
+        #     list["two"].append(each)   
+        # elif respcode in range3 :
+        #     list["three"].append(each)   
+        # elif respcode in range4 :
+        #     list["four"].append(each)   
+        # elif respcode in range5 :
+        #     list["five"].append(each)                       
 
-    return list
+    return resplist
 
 
 
